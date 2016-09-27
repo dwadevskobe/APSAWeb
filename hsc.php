6eace993e70fd71626cc877e0f2abf4006a9bfbe
Steve Wang <steve_wang3@yahoo.com>
2016-09-26 19:13:26 -0700

    Update

--- hsc.php
+++ hsc.php
@@ -31,6 +31,24 @@
   <a class = "tablinks" onclick="newTab(event)" href="cu.php">Contact Us</a></li>
 </div>
 
+<div class ="center">
+      <div class = "intro1" style = "margin-top:25px">
+        <div class = "color1 shadow" style = "margin:15px; padding:10px">
+        	<p> So what's a high school conference? A high school conference is a whole day event dedicated to exposing high school students (9th-12th grades) to higher education through interactive workshops addressing topics such as ethnic/cultural identity, self-expression & art, financial aid, political awareness, high school and community leadership, and youth empowerment. There will also be motivational speakers to share their experiences and meaningful interaction with both UCSD student mentors ("Funcilitators") and each other. Did I mention that your participation in the high school conference is completely FREE and includes FREE meals???  
+        	</p>
+
+        	<p> What is included? </p>
+         
+        		<li> Breakfast, lunch, DINNER & snacks will be provided! </li>
+        		<li> A tour of the UCSD campus </li> 
+				<li> All attendees will get a free t-shirt </li> 
+                <li> Making new friends! </li>
+                <li> All attending HS seniors will have the opportunity of winning a scholarship ($$$) good for any college/university. </li>
+      
+        </div>
+    </div>
+</div>
+
 </body>
 
 </html>
