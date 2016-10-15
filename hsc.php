b6fb6ba22422bdeebc1cf86cd5bd5bfb86ba01dc
Steve Wang <steve_wang3@yahoo.com>
2016-10-15 13:38:45 -0700

    Updated

--- hsc.php
+++ hsc.php
@@ -31,22 +31,21 @@
   <a class = "tablinks" onclick="newTab(event)" href="cu.php">Contact Us</a></li>
 </div>
 
-<div class ="center">
       <div class = "intro1" style = "margin-top:25px">
         <div class = "color1 shadow" style = "margin:15px; padding:10px">
-          <h1> Saturday, November 12, 2016  8:00AM - 9:00PM </h1>
+          <h1 class = "center"> Saturday, November 12, 2016  8:00AM - 9:00PM </h1>
         	<p> So what's a high school conference? A high school conference is a whole day event dedicated to exposing high school students (9th-12th grades) to higher education through interactive workshops addressing topics such as ethnic/cultural identity, self-expression & art, financial aid, political awareness, high school and community leadership, and youth empowerment. There will also be motivational speakers to share their experiences and meaningful interaction with both UCSD student mentors ("Funcilitators") and each other. Did I mention that your participation in the high school conference is completely FREE and includes FREE meals???  
         	</p>
 
         	<p> What is included? </p>
          
-            <ul>
+
         		<li> Breakfast, lunch, DINNER & snacks will be provided! </li>
         		<li> A tour of the UCSD campus </li> 
 				    <li> All attendees will get a free t-shirt </li> 
             <li> Making new friends! </li>
             <li> All attending HS seniors will have the opportunity of winning a scholarship ($$$) good for any college/university. </li>
-            </ul>
+
 
         <h2> If you want to sign up to be a funcilitator, applications can be found here: More info inside the link.
         <a href = https://docs.google.com/forms/d/e/1FAIpQLSdsn5fHgiwqIlB_LoHoQLBsSfQZt7fkajQicQfOFcdlBEKgYg/viewform> Funsies Signup App </a>
@@ -55,7 +54,6 @@
 
 
 
-    </div>
 </div>
 
 </body>
