e398f62137e00dc1a390ec4c830f191fb015261d
Steve Wang <steve_wang3@yahoo.com>
2016-11-07 20:24:38 -0800

    Updated

--- cal.php
+++ cal.php
@@ -43,7 +43,7 @@
 
 <p>  <br> </p>
 
-<iframe src="https://calendar.google.com/calendar/embed?src=ucsdapsa%40gmail.com&ctz=America/Los_Angeles" style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
+<iframe src="https://calendar.google.com/calendar/embed?src=ucsdapsa%40gmail.com&ctz=America/Los_Angeles" style="border: 0" width="100%" height="650" frameborder="0" scrolling="no"></iframe>
 
 </body>
 </html>
