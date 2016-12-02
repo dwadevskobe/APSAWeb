58bc1da93a0e97c45b9f4b06265efbe6639122b1
Steve Wang <steve_wang3@yahoo.com>
2016-12-01 23:41:56 -0800

    Updated

--- media.php
+++ media.php
@@ -46,26 +46,7 @@
 <div class ="center">
       <div class = "intro1" style = "margin-top:20px;">
         <div class = "color1 shadow" style = "margin:15px; padding:10px">
-          <h2> Media </h2>
-          <p><span style="font-weight:bold"> Q: What is APSA?</span> <br>
-A: APSA stands for Asian Pacific-Islander Student Alliance. It is a student organization that focuses on the issues about the API (Asian Pacific-Islander) community. We focus on four aspects: Cultural, Academic, Political, Social or CAPS. <br><br><br>
-
-
-Q: <span style="font-weight:bold">How do I learn more about your organization?</span> <br>
-A: You can start by attending one of our GBMs to get to know us a little bit better. We also table during the 1st week of every quarter on Library Walk, so if you see us you should stop by and request more information. And you can always just contact us by email.  <br><br><br>
-
-
-Q: <span style="font-weight:bold">When and where can I attend your GBMs?</span> <br>
-A: We meet every even week (Week 2, 4, 6, etc.) on Tuesdays at 6:30PM. The location will always be in the CCC Large (Cross-Cultural Center Large). <br><br><br>
-
-Q: <span style="font-weight:bold">How do I become a member? </span><br>
-A: You don't have to be! There is no application needed! <br><br><br>
-
-Q: <span style="font-weight:bold">I am not a member of APSA. Can I still attend your events? </span> <br>
-A: Yes! Events, such as GBM retreats, socials, and biggie events, can be attended by anyone as long as they sign up on the events we create on Facebook! <br><br><br>
-
-Q:<span style="font-weight:bold"> It is currently the middle of the quarter or the school year. Will I still be able to join APSA? </span> <br>
-A: Yes! You can join at any time during any quarter. The sooner, the better to give our love and support to you!</p>
+          <h1> Picture Albums Posted soon!! </h1>
         </div>
       </div>
     </div>
