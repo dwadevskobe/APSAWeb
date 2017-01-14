92eaa4601c73c2ea5f0e0c4be7f4295380bdbc1c
Steve Wang <steve_wang3@yahoo.com>
2017-01-13 16:00:28 -0800

    Updated

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ UCSD APSA
                                      	<a href="https://www.facebook.com/groups/ucsdapsa/"><h3 class = "signup">Follow our Group</h3></a> &nbsp
                                      	<a href="https://www.facebook.com/apsaucsd/?fref=ts"><h3 class ="signup">Follow our Page</h3></a>
                                      </li>
-                                     <li class = "snap1"> <h3 class = "signup"> &nbsp Follow us on Instagram for live updates : ucsdapsa  </h3></li>
+                                     <li class = "snap"> <h3 class = "signup"> &nbsp Follow us on Instagram for live updates : ucsdapsa  </h3></li>
                                 </ul>
 
                                 <p> Signup for Email Updates!! <br>
